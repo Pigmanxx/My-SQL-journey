168368f858b73a56d90370d8db1a2db77263e541
ScottZRY <renyan.zhang@shms-mail.ch>
2022-10-06 22:38:52 -0400

    sql

--- 6 The JOIN operation.sql
+++ 6 The JOIN operation.sql
@@ -68,5 +68,3 @@ SELECT mdate,
   SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
   FROM game LEFT JOIN goal ON matchid = id
   GROUP BY mdate, matchid, team1, team2;
-
--- 14.
--- 7 More JOIN operations.sql
+++ 7 More JOIN operations.sql
@@ -8,3 +8,35 @@ SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane';
 
+-- 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr;
+
+-- 4. What id number does the actor 'Glenn Close' have?
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close';
+
+-- 5. What is the id of the film 'Casablanca'
+SELECT id
+FROM movie
+WHERE title = 'Casablanca';
+
+-- 6. Obtain the cast list for 'Casablanca'.
+SELECT name
+FROM actor JOIN casting ON actorid = id
+WHERE movieid = 27;
+
+-- 7. Obtain the cast list for the film 'Alien'
+SELECT name
+FROM actor JOIN casting ON actorid = id
+WHERE movieid = (SELECT id FROM movie WHERE title = 'Alien');
+
+-- 8. List the films in which 'Harrison Ford' has appeared
+SELECT title
+FROM movie JOIN casting ON movieid = movie.id
+WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford');
+
+-- 9. List the films where 'Harrison Ford' has appeared - but not in the starring role.
